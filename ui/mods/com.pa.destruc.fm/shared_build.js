ffd058b2aab24454bce191456707b8ddd0e65219
Oliver <158805248+OliverEvans08@users.noreply.github.com>
2025-05-05 21:57:49 +1000

    this

--- ui/mods/com.pa.destruc.fm/shared_build.js
+++
@@ -1,7 +0,0 @@
-var newBuild = {
-"/pa/units/orbital/orbital_carrier/orbital_carrier.json": ["orbital", 0,{ row: 0, column: 3, titans: true }],
-
-}
-if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
-_.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
-}
